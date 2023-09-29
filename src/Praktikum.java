bca07aa6c869672136556c94ba010e5b22ca785f
Maxx <maxx@aolc.ru>
2023-09-29 10:06:40 +0700

    Final version

--- src/Praktikum.java
+++ src/Praktikum.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
+
 public class Praktikum {
     public static void main(String[] args) {//***Main
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите год:");
         int year = scanner.nextInt();
         if (isLeapYear(year)) {
-            System.out.println("Високосный");// здесь нужно вывести результат
+            System.out.println("Високосный");
         } else {
             System.out.println("Обычный короткий год");
         }
     }//***End of Main
+
     public static boolean isLeapYear(int year) {// ***Проверка на високосность
         if (year % 4 == 0){
             if (year % 100 != 0 || year % 400 == 0){
